06b0348b328957281696310358948207ad3ea04a
Filip Gerek <filip.gerek@student.tuke.sk>
2026-07-06 22:37:34 +0200

    Add logout command to CLI for user session management; integrate logout functionality into main app.

--- src/dexcom_cli/cli/main.py
+++ src/dexcom_cli/cli/main.py
@@ -1,12 +1,13 @@
 import typer
 from dexcom_cli.cli.glucose import app as glucose_app
 from dexcom_cli.cli.login import app as login_app
-
+from dexcom_cli.cli.logout import app as logout_app
 
 app = typer.Typer()
 
 app.add_typer(glucose_app, name="glucose")
 app.add_typer(login_app, name="login")
+app.add_typer(logout_app, name="logout")
 
 if __name__ == "__main__":
     app()
